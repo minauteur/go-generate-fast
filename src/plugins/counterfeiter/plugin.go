96f65725b4651c2f0bf513ecf8015dc37f16f410
Thom Pennington <thomas.pennington@bayer.com>
2025-07-28 22:36:25 -0500

    add error messages

--- src/plugins/counterfeiter/plugin.go
+++ src/plugins/counterfeiter/plugin.go
@@ -9,14 +9,12 @@ import (
 	"go.uber.org/zap"
 )
 
-const genRegex = `\/\/counterfeiter:generate\s+(.+)`
 const packageRegex = `(?m)^package\s+([a-zA-Z_][a-zA-Z0-9_]*)`
 const inRegex = `(?m)^//\s*([a-zA-Z0-9_\-]+\.go)\b`
 
-var genRe, pkgRe, inRe *regexp.Regexp
+var pkgRe, inRe *regexp.Regexp
 
 func init() {
-	genRe = regexp.MustCompile(genRegex)
 	pkgRe = regexp.MustCompile(packageRegex)
 	inRe = regexp.MustCompile(inRegex)
 	plugins.RegisterPlugin(&CounterfeiterPlugin{})
@@ -47,14 +45,26 @@ func (p *CounterfeiterPlugin) ComputeInputOutputFiles(opts plugins.GenerateOpts)
 		return nil
 	}
 
-	pkg := pkgRe.Find(bytes)
-	zap.S().Debug("counterfeiter: found package name in file: ", string(pkg))
-	trimPackage := strings.TrimPrefix(string(pkg), "package ")
-	ioFiles.OutputPatterns = []string{trimPackage + "fakes/*.go"}
+	pkgDec := pkgRe.Find(bytes)
+	if len(pkgDec) == 0 {
+		zap.S().Errorf("counterfeiter: cannot find package declaration in %s", opts.Path)
+		return nil
+	}
+	pkgName := strings.TrimPrefix(string(pkgDec), "package ")
+
+	ioFiles.OutputPatterns = []string{pkgName + "fakes/*.go"}
 
 	inputs := inRe.FindAllStringSubmatch(string(bytes), -1)
+	if inputs == nil {
+		zap.S().Errorf("counterfeiter: cannot find input files in %s", opts.Path)
+		return nil
+	}
+
 	for _, input := range inputs {
-		zap.S().Debugf("counterfeiter: found input file: %s", input[1])
+		if len(input) < 2 {
+			zap.S().Errorf("counterfeiter: invalid input match in %s: %v", opts.Path, input)
+			return nil
+		}
 		ioFiles.InputFiles = append(ioFiles.InputFiles, input[1])
 	}
 
